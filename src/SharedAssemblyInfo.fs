b9ed576c0163cfe28bc2db1ca85e77752039447e
Matthias Dittrich <matthi.d@googlemail.com>
2015-12-24 12:35:46 +0100

    Bump version to 2.0.1

--- src/SharedAssemblyInfo.fs
+++ src/SharedAssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyCompanyAttribute("Yaaf.Logging")>]
 [<assembly: AssemblyProductAttribute("Yaaf.Logging")>]
 [<assembly: AssemblyCopyrightAttribute("Yaaf.Logging Copyright © Matthias Dittrich 2011-2015")>]
-[<assembly: AssemblyVersionAttribute("2.0.0")>]
-[<assembly: AssemblyFileVersionAttribute("2.0.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.0.0")>]
+[<assembly: AssemblyVersionAttribute("2.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("2.0.1")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.0.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.0.0"
+    let [<Literal>] Version = "2.0.1"
